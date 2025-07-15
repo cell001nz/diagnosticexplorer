e715cb8f21a8c26d962f802011944cb75323962f
cell001nz <cameronelliot@gmail.com>
2025-07-16 11:32:06 +1200

    api/DataTrigger to AuthorizationLevel.User

--- web/api/DataTrigger.cs
+++ web/api/DataTrigger.cs
@@ -15,7 +15,7 @@ public class DataTrigger
     }
 
     [Function("DataTrigger")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.User, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
